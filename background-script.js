a3516894ec7065b051d2cc35e5ff72f5efc678c9
AviaAlex <70788432+AviaAlex@users.noreply.github.com>
2022-11-11 12:11:28 -0500

    Update background-script.js

--- background-script.js
+++ background-script.js
@@ -1,9 +1,8 @@
 function onActivation() {
   window.location.replace("about:blank")
   const newdiv = document.createElement("div")
-  const newtext = "Sorry, access denied."
-  const newtextappended = document.createTextNode(newtext)
-  newdiv.appendChild(newtextappended)
+  const divcontent = document.createTextNode("Sorry, access denied.")
+  newdiv.appendChild(divcontent)
 }
 window.addEventListener('click', (event) => {
   onActivation() // Redirect them to the no access page.
