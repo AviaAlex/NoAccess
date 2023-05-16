77b89edbcaebfd3dd4896f951cc420a9f166c85e
AviaAlex <70788432+AviaAlex@users.noreply.github.com>
2023-05-16 14:02:25 -0400

    Update background-script.js

--- background-script.js
+++ background-script.js
@@ -1,9 +1,9 @@
 function onActivation() {
-  window.location.replace("about:blank")
-  const newdiv = document.createElement("div")
-  const divcontent = document.createTextNode("Sorry, access denied.")
-  newdiv.appendChild(divcontent)
+  window.location.replace("about:blank");
+  const newdiv = document.createElement("div");
+  const divcontent = document.createTextNode("Sorry, access denied.");
+  newdiv.appendChild(divcontent);
 }
 window.addEventListener('click', (event) => {
-  onActivation() // Redirect them to the no access page.
+  onActivation(); // Redirect them to the no access page.
 })
